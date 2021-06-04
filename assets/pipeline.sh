33e8d3b529c6ff5aa6c3e37e26e5c579c2dcdade
wa-bha <bhavit.w04@gmail.com>
2021-06-04 16:37:54 +1200

    Changes to pipeline.sh file

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -9,7 +9,7 @@ npm run build
 # If the build was unsuccessful, halt
 if [ $? == 0 ] 
 then
-	echo Application Successfuly built using NPM
+	echo Application built successfully using NPM
 else
 	echo Application build unsuccessful
 	exit 2
@@ -23,9 +23,9 @@ cd ../
 # Run git commands (add to repo, commit, push)
 # Commit using supplied commit message ($1)
 git add .
-git commit -m $1
+read -p "Provide commit message" message
+git commit -m "$message"
 git push origin master
-git status
 
 # Check status of git repo
 git status
